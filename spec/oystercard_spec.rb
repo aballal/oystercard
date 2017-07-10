8a1e625f8ba45087db5af6ac2fba2c58ee592254
Imogen Hardy <imogen.hardy@gmail.com>
2017-07-10 16:27:57 +0100

    Challenge 4: Adding balance attribute to Oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,17 @@
 require 'oystercard'
 
 describe Oystercard do
+  subject(:card) { described_class.new }
 
+  it { is_expected.to respond_to(:balance) }
+
+  describe '#new' do
+    it "takes one argument at initialization" do
+      expect(Oystercard).to respond_to(:new).with(1).argument
+    end
+
+    it "should create a card with zero balance" do
+      expect(card.balance).to eq 0
+    end
+  end
 end
