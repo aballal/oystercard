e2fb306379bc5cae26389157a703b01d04ac66d8
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-12 12:11:25 +0100

    Completed 13, Added name and zone to station

---
+++ feature_spec/feature9_spec.rb
@@ -0,0 +1,9 @@
+# In order to know how far I have travelled
+# As a customer
+# I want to know what zone a station is in
+
+require_relative '../lib/station.rb'
+
+station = Station.new("Kings Cross",1)
+p station.name
+p station.zone
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,16 @@
 require 'station'
 
 describe Station do
+  subject(:station) {described_class.new("Kings Cross", 1)}
+
+  describe '#new' do
+    it 'can be named when created' do
+      expect(station.name).to eq "Kings Cross"
+    end
+
+    it 'can have a zone when created' do
+      expect(station.zone).to eq 1
+    end
+  end
 
 end
