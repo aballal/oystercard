a67f740d51b8285b693f465d84cec399dd627ab5
Eoin O Sullivan <esully666@gmail.com>
2017-07-12 13:59:09 +0000

    Step 14

--- feature_spec/feature10_spec.rb
+++ feature_spec/feature10_spec.rb
@@ -1,3 +1,17 @@
 require_relative '../lib/journey.rb'
 
+require_relative '../lib/oystercard.rb'
+require_relative '../lib/station.rb'
+
+card = Oystercard.new
+card.top_up(12)
+station1 = Station.new("Kings cross", 1)
+station2 = Station.new("Fish market", 3)
+
 journey = Journey.new
+
+card.touch_in(station1)
+p journey.entry_station.name
+card.touch_out(station2)
+p journey.exit_station.name
+
--- lib/journey.rb
+++ lib/journey.rb
@@ -1,3 +1,12 @@
 class Journey
-
+   attr_reader :entry_station, :exit_station
+    def initialize(entry_station, exit_station = nil)
+        @entry_station = entry_station
+        @exit_station = exit_station
+    end
+    
+    def set_exit_station(station)
+        @exit_station = station
+    end
+  
 end
