56bb7b8f1233e39556283ac1df5e970432e91593
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-13 17:10:11 +0100

    Added JourneyLog tests and code for initialization, start, finish and current journey

---
+++ lib/journey_log.rb
@@ -0,0 +1,28 @@
+require 'journey'
+
+class JourneyLog
+
+  attr_reader :journeys
+
+  def initialize(journey_class = Journey)
+    @journey_class = journey_class
+    @journeys = []
+  end
+
+  def start(station)
+    journey = @journey_class.new(station, nil)
+    @journeys << journey
+    @journeys.last.entry_station
+  end
+
+  def finish(station)
+    current_journey.set_exit_station(station)
+  end
+
+  private
+
+  def current_journey
+    (@journeys.empty? || @journeys.last.complete?) ? @journey_class.new : @journeys.last
+  end
+
+end
---
+++ spec/journey_log_spec.rb
@@ -0,0 +1,24 @@
+require 'journey_log'
+
+describe JourneyLog do
+  subject(:journey_log) { described_class.new(Journey) }
+  subject(:journey) {described_class.new(station1, station2)}
+  let(:station1) {double(:station, :name => "Kings Cross", :zone => 1)}
+  let(:station2) {double(:station, :name => "Canary Wharf", :zone => 2)}
+
+  it 'has no journeys to begin with' do
+    expect(journey_log.journeys).to be_empty
+  end
+
+  it 'can start a journey' do
+    journey_log.start(station1)
+    expect(journey_log.journeys.last.entry_station).to eq station1
+  end
+
+  it 'can finish a journey' do
+    journey_log.start(station1)
+    journey_log.finish(station2)
+    expect(journey_log.journeys.last.exit_station).to eq station2
+  end
+
+end
