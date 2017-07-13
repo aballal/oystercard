d15de38fd666896afc1c457f4c0b7f4ce29b7962
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-13 06:05:15 +0100

    Removed attribute reader for fare as there is a custom method

--- lib/journey.rb
+++ lib/journey.rb
@@ -3,7 +3,7 @@ class Journey
   MIN_FARE = 1
   PENALTY_FARE = 6
 
-  attr_reader :entry_station, :exit_station, :fare
+  attr_reader :entry_station, :exit_station
 
   def initialize(entry_station = nil, exit_station = nil)
     @entry_station = entry_station
